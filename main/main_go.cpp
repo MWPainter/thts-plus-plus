4dfb1c23e1d4e7fa31dcd394c3a5ead0333a20f6
Michael Painter <michaelpainter.1994@gmail.com>
2023-05-12 11:43:24 +0100

    Tuning for go

--- main/main_go.cpp
+++ main/main_go.cpp
@@ -253,16 +253,16 @@ int main(int argc, char* argv[]) {
         string alg_id = ALG_ID_DENTS;
 
         shared_ptr<thts::GoAlgParams> alg_params = make_shared<thts::GoAlgParams>();
-        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP, 25.0);         // use result from EXPR_ID_EST_HPS
-        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP_OPP, 25.0);     // use result from EXPR_ID_EST_HPS
-        alg_params->insert_or_assign(PARAM_DECAY_TEMP_ROOT_NODE_VISITS_SCALE, 0.01);      
-        alg_params->insert_or_assign(PARAM_DECAY_TEMP_ROOT_NODE_VISITS_SCALE_OPP, 0.01);   
-        // alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE, 0.05);      
-        // alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE_OPP, 0.05);               
+        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP, 35.0);         // use result from EXPR_ID_EST_HPS
+        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP_OPP, 35.0);     // use result from EXPR_ID_EST_HPS
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_ROOT_NODE_VISITS_SCALE, 0.003);      
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_ROOT_NODE_VISITS_SCALE_OPP, 0.003);   
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE, 0.05);      
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE_OPP, 0.05);               
         alg_params->insert_or_assign(PARAM_PRIOR_COEFF, 0.5);            
         alg_params->insert_or_assign(PARAM_PRIOR_COEFF_OPP, 0.5);       
-        // alg_params->insert_or_assign(PARAM_DECAY_TEMP_USE_SIGMOID, 1.0);
-        // alg_params->insert_or_assign(PARAM_DECAY_TEMP_USE_SIGMOID_OPP, 1.0);  
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_USE_SIGMOID, 1.0);
+        alg_params->insert_or_assign(PARAM_DECAY_TEMP_USE_SIGMOID_OPP, 1.0);  
         alg_params->insert_or_assign(PARAM_INIT_DECAY_TEMP, temp);                
         alg_params->insert_or_assign(PARAM_INIT_DECAY_TEMP_OPP, temp_opp);             
         alg_params->insert_or_assign(PARAM_MENTS_ROOT_EPS, 1.0);                                    
@@ -298,9 +298,7 @@ int main(int argc, char* argv[]) {
 
         shared_ptr<thts::GoAlgParams> alg_params = make_shared<thts::GoAlgParams>();
         alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP, temp);
-        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP_OPP, temp_opp);   
-        alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE, 0.05);      
-        alg_params->insert_or_assign(PARAM_DECAY_TEMP_VISITS_SCALE_OPP, 0.05);               
+        alg_params->insert_or_assign(PARAM_BIAS_OR_SEARCH_TEMP_OPP, temp_opp);                
         alg_params->insert_or_assign(PARAM_PRIOR_COEFF, 0.5);            
         alg_params->insert_or_assign(PARAM_PRIOR_COEFF_OPP, 0.5);       
         alg_params->insert_or_assign(PARAM_DECAY_TEMP_USE_SIGMOID, 1.0);
