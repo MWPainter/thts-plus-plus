624ba88442829bdd8349fdceb2901437a0e0c690
MWPainter <michaelpainter.1994@gmail.com>
2023-07-27 06:59:17 +0100

    Update run_go.cpp - optimising for jade

--- src/go/run_go.cpp
+++ src/go/run_go.cpp
@@ -552,6 +552,7 @@ namespace thts {
             manager_args.root_node_epsilon = 0.67;
             manager_args.shift_pseudo_q_values = true;
             manager_args.prior_policy_search_weight = 0.5;
+            manager_args.value_temp_init = 0.0;
             if (alg_params != nullptr) {
                 if (!is_opp) {
                     if (contains_key(alg_params, PARAM_BIAS_OR_SEARCH_TEMP)) {
