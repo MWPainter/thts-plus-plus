e6c0d3887e48fd14d6f7a600453f79d3b9058d45
Michael Painter <michaelpainter.1994@gmail.com>
2023-08-25 19:04:11 +0100

    Didnt save this file when commiting whoops

--- slurm/spawn_x02_bts_tune_temp_compare.sh
+++ slurm/spawn_x02_bts_tune_temp_compare.sh
@@ -1,7 +1,7 @@
 # variables 
-EXPR_ID="x01_bts_tune_temp_sqrt_decay"
+EXPR_ID="x02_bts_tune_temp_compare"
 
-temps=("100.0" "30.0" "10.0" "3.0" "1.0" "0.3")
+temps=("0.0" "1.0")
 
 for AGENT_ONE_ID in ${temps[@]}
 do
@@ -12,7 +12,7 @@ do
             OUTDIR=slurm_output/${EXPR_ID}
             mkdir -p $OUTDIR
             OUTFILE=slurm_output/${EXPR_ID}/9x9_${AGENT_ONE_ID}_vs_${AGENT_TWO_ID}.out
-            sbatch --job-name=1:${AGENT_ONE_ID}vs${AGENT_TWO_ID} --output=$OUTFILE --export=EXPR_ID=$EXPR_ID,AGENT_ONE_ID=$AGENT_ONE_ID,AGENT_TWO_ID=$AGENT_TWO_ID run_go.slurm
+            sbatch --job-name=2:${AGENT_ONE_ID}vs${AGENT_TWO_ID} --output=$OUTFILE --export=EXPR_ID=$EXPR_ID,AGENT_ONE_ID=$AGENT_ONE_ID,AGENT_TWO_ID=$AGENT_TWO_ID run_go.slurm
         fi
     done
 done
