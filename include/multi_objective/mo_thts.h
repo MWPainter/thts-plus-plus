f2a7839b5b9edcc61804bd069b821750de2d1324
Michael Painter <michaelpainter.1994@gmail.com>
2023-08-28 19:15:06 +0100

    Added core multi_objective implementation to library

---
+++ include/multi_objective/mo_thts.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "thts_pool.h"
+#include "thts_decision_node.h"
+#include "thts_env_context.h"
+#include "thts_logger.h"
+#include "thts_manager.h"
+
+#include <chrono>
+#include <condition_variable>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include <Eigen/Dense>
+
+
+namespace thts {
+    /**
+     * A class encapsulating all of the logic required to run a thts routine on a multi-objective environment.
+     * 
+     * See ThtsPool for a full description of this class
+     * 
+     * This multi-objective version just replaces the 'double' 
+     * 
+     * 
+     */
+    class ThtsPool {
+
+        public:
+            /**
+             * Constructs the MoThtsPool with 'num_threads' worker threads.
+             * 
+             * Args:
+             *      manager: The ThtsManager to use for this instance of thts
+             *      root_node: 
+             *          The root node to run thts on. The algorithm is specified by the subclass of ThtsDNode used. If 
+             *          NULL, then a default root node construction is attempted using the initial state from 
+             *          thts_manager->thts_env.
+             *      num_threads: The number of worker threads to spawn
+             */
+            MoThtsPool(
+                std::shared_ptr<ThtsManager> thts_manager=nullptr, 
+                std::shared_ptr<ThtsDNode> root_node=nullptr, 
+                int num_threads=1,
+                std::shared_ptr<ThtsLogger> logger=nullptr);
+
+            /**
+             * Destructor. (Should just call ~ThtsPool()).
+             */
+            virtual ~MoThtsPool() = default;
+
+        protected:
+
+            /**
+             * Runs the selection phase of a trial, called by worker threads.
+             * 
+             * Duplicate of ThtsPool::run_backup_phase, but updating types to use VectorXd rewards, rather than double.
+             * This also needs to call get_mo_reward_itfc, rather than get_reward_itfc
+             * 
+             * Ideally we would have a common implementation (using templates somehow), but it seems to make the code a 
+             * bit obfuscated. So just accepting duplicated code for now :/
+             */
+            void run_selection_phase(
+                std::vector<std::pair<std::shared_ptr<ThtsDNode>,std::shared_ptr<ThtsCNode>>>& nodes_to_backup, 
+                std::vector<Eigen::VectorXd>& rewards, 
+                ThtsEnvContext& context);
+
+            /**
+             * Runs the backup phase of a trial, called by worker threads.
+             * 
+             * Duplicate of ThtsPool::run_backup_phase, but updating types to use VectorXd rewards, rather than double
+             * 
+             * Ideally we would have a common implementation (using templates somehow), but it seems to make the code a 
+             * bit obfuscated. So just accepting duplicated code for now :/
+             */
+            void run_backup_phase(
+                std::vector<std::pair<std::shared_ptr<ThtsDNode>,std::shared_ptr<ThtsCNode>>>& nodes_to_backup, 
+                std::vector<Eigen::VectorXd>& rewards, 
+                ThtsEnvContext& context);
+
+            /**
+             * Performs a single thts trial. Called by worker_fn.
+             * 
+             * Duplicates ThtsPool::run_thts_trial, but updates types to use VectorXd rewards.
+             * 
+             * Ideally we would have a common implementation (using templates somehow), but it seems to make the code a 
+             * bit obfuscated. So just accepting duplicated code for now :/
+             */
+            virtual void run_thts_trial(int trials_remaining);          
+    };
+}
---
+++ include/multi_objective/mo_thts_env.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "thts_env.h"
+#include "thts_env_context.h"
+#include "thts_types.h"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <Eigen/Dense>
+
+namespace thts {
+    // Forward declare
+    class RandManager;
+    
+    /**
+     * An abstract class for representing an multi-objective environment.
+     */
+    class MoThtsEnv : public ThtsEnv {
+        public:
+            /**
+             * Constructor
+             */
+            MoThtsEnv(bool is_fully_observable);
+            
+            /**
+             * Override ThtsEnv get_reward_itfc to make it throw an exception. It shouldn't be called by multi-objective
+             * implementations.
+             */
+            virtual double get_reward_itfc(
+                std::shared_ptr<const State> state, 
+                std::shared_ptr<const Action> action, 
+                std::shared_ptr<const Observation> observation=nullptr) const;
+            
+            /**
+             * Returns the multi objective reward for a given state, action, observation tuple.
+             * 
+             * Commonly the reward is written as a function of just the state and action pair. But we provide the 
+             * option to depend on the observation too. 
+             * 
+             * Args:
+             *      state: The current state to get a reward for
+             *      action: The action taken to get a reward for
+             *      observation: 
+             *          The (optional) observation sampled from the state, action pair that can optionally be used as 
+             *          part of the reward function.
+             * 
+             * Returns:
+             *      The reward for taking 'action' from 'state' (and sampling 'observation')
+             */
+            virtual Eigen::VectorXd get_mo_reward_itfc(
+                std::shared_ptr<const State> state, 
+                std::shared_ptr<const Action> action, 
+                std::shared_ptr<const Observation> observation=nullptr) const = 0;
+    };
+}
