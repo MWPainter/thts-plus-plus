41f1b4f081286521cee9732467856e3c35dccd20
Michael Painter <michaelpainter.1994@gmail.com>
2023-08-28 16:39:29 +0100

    Slurming

---
+++ slurm/spawn_x08_puct_thread_test.sh
@@ -0,0 +1,18 @@
+# variables 
+EXPR_ID="x08_puct_tune_threads"
+
+threads=("16" "32" "64" "128")
+
+for AGENT_ONE_ID in ${threads[@]}
+do
+    for AGENT_TWO_ID in ${threads[@]}
+    do
+        if [[ $AGENT_ONE_ID != $AGENT_TWO_ID ]]
+        then
+            OUTDIR=slurm_output/${EXPR_ID}
+            mkdir -p $OUTDIR
+            OUTFILE=slurm_output/${EXPR_ID}/9x9_${AGENT_ONE_ID}_vs_${AGENT_TWO_ID}.out
+            sbatch --job-name=8:${AGENT_ONE_ID}vs${AGENT_TWO_ID} --output=$OUTFILE --export=EXPR_ID=$EXPR_ID,AGENT_ONE_ID=$AGENT_ONE_ID,AGENT_TWO_ID=$AGENT_TWO_ID run_go.slurm
+        fi
+    done
+done
