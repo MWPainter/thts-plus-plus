9acad7706a0806ca79c5bb52841ec76967cea51e
Michael Painter <michaelpainter.1994@gmail.com>
2023-10-26 03:00:39 +0100

    Whoops

--- slurm/spawn_y030_extend.sh
+++ slurm/spawn_y030_extend.sh
@@ -31,3 +31,17 @@ do
         fi
     done
 done
+
+for AGENT_ONE_ID in ${agent_ids_new[@]}
+do
+    for AGENT_TWO_ID in ${agent_ids_new[@]}
+    do
+        if [[ $AGENT_ONE_ID != $AGENT_TWO_ID ]]
+        then
+            OUTDIR=slurm_output/${EXPR_ID}
+            mkdir -p $OUTDIR
+            OUTFILE=slurm_output/${EXPR_ID}/19x19_${AGENT_ONE_ID}_vs_${AGENT_TWO_ID}.out
+            sbatch --job-name=y30:${AGENT_ONE_ID}vs${AGENT_TWO_ID} --output=$OUTFILE --export=EXPR_ID=$EXPR_ID,AGENT_ONE_ID=$AGENT_ONE_ID,AGENT_TWO_ID=$AGENT_TWO_ID run_go.slurm
+        fi
+    done
+done
