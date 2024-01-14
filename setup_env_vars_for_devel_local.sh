9ab0463e3ff714085192780403f605fdceac2afa
MWPainter <michaelpainter.1994@gmail.com>
2024-01-14 09:00:33 +0000

    quality of life for remote dev

--- setup_env_vars_for_devel_local.sh
+++ setup_env_vars_for_devel_local.sh
@@ -2,5 +2,5 @@ dirname=$(readlink -f -- $(dirname "${BASH_SOURCE[0]}"))
 export PYTHONPATH=$dirname:$dirname/py:$PYTHONPATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$dirname
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$dirname/external/qhull/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/jmain02/home/J2AD008/wga37/mmp10-wga37/anaconda3/envs/thts3.12/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/michael/anaconda3/envs/thts3.12/lib
 
