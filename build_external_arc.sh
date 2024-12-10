03f2ff3347fd767bcc9cf4014a641a1085e672ba
Michael Painter <michaelpainter.1994@gmail.com>
2024-12-10 05:05:59 +0000

    Jumped the gun, need to compile all of the externals with the flag too -.-

--- build_external_arc.sh
+++ build_external_arc.sh
@@ -4,13 +4,13 @@ export Boost_INCLUDE_DIR=/home/pemb5587/cpp_include/
 cd $dirname/external/googletest
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=. -DCMAKE_CXX_FLAGS='-fPIE'
+cmake .. -DCMAKE_INSTALL_PREFIX=. -DCMAKE_CXX_FLAGS='-fPIE -D_GLIBCXX_USE_CXX11_ABI=0'
 make
 make install
 
 cd $dirname/external/qhull
 mkdir qhull_build
-cmake .. -DCMAKE_INSTALL_PREFIX=. -DCMAKE_CXX_STANDARD=20
+cmake .. -DCMAKE_INSTALL_PREFIX=. -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0'
 make install
 
 cd $dirname/external
@@ -24,5 +24,5 @@ chmod u+x coinbrew
 cd $dirname/external/bayesopt
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=. -DBoost_INCLUDE_DIR=$Boost_INCLUDE_DIR -DCMAKE_CXX_STANDARD=20
+cmake .. -DCMAKE_INSTALL_PREFIX=. -DBoost_INCLUDE_DIR=$Boost_INCLUDE_DIR -DCMAKE_CXX_STANDARD=20 -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0'
 make install
