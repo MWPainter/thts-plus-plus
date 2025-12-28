76b257411094cb2cae09a359405b62f78736f4ab
Michael Painter <michaelpainter.1994@gmail.com>
2025-12-28 12:41:15 +0000

    Forgot to add hpopt 301 to list of configs

--- main_aux/configs/hpopt_config.h
+++ main_aux/configs/hpopt_config.h
@@ -9,6 +9,7 @@
 #include "main_aux/configs/hpopt_config_001_debug.h"
 
 #include "main_aux/configs/hpopt_config_300_frozen_lake_det_dense.h"
+#include "main_aux/configs/hpopt_config_301_frozen_lake_det_dense.h"
 #include "main_aux/configs/hpopt_config_310_frozen_lake_det_sparse.h"
 #include "main_aux/configs/hpopt_config_320_frozen_lake_stoch_dense.h"
 #include "main_aux/configs/hpopt_config_330_frozen_lake_stoch_sparse.h"
@@ -23,6 +24,7 @@ static const std::vector<std::vector<HpoptConfigMap>> ALL_HPOPT_CONFIGS =
 {
     HPOPT_CONFIG_001,
     HPOPT_CONFIG_300,
+    HPOPT_CONFIG_301,
     HPOPT_CONFIG_310,
     // HPOPT_CONFIG_320,
     HPOPT_CONFIG_330,
