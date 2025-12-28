336ef3710e657e9f9eedb0747ffcf8f668e18dd3
Michael Painter <michaelpainter.1994@gmail.com>
2025-12-28 13:13:49 +0000

    Updated early stopping to make a bit more strict as it sped up hpopt significantly

--- main_aux/hpopt_manager.cpp
+++ main_aux/hpopt_manager.cpp
@@ -386,8 +386,8 @@ namespace thts {
                 << ", std_mean_eval=" << std_mean_eval << " >? " << estimate_confidence_threshold << endl;
 
             // Early stopping: stop run more than min repeats and cleanly worse than best 
-            // i.e. if confidence intervals don't overlap (1.3 std ≈ 90% CI, so <1% chance of error)
-            constexpr double early_stop_z = 1.3;
+            // i.e. if confidence intervals don't overlap (1.65 std ≈ 95% CI, so <0.1% chance of error)
+            constexpr double early_stop_z = 1.65;
             if (repeats_run >= min_repeats && 
                 mean_eval + early_stop_z * std_mean_eval < best_mean_eval - early_stop_z * best_std_mean_eval)
             {
